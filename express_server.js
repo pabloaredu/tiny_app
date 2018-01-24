e8b834d1ba3b5b2aa74e685889812caffaf15ff5
Pablo Aredu <pablo.adu83@gmail.com>
2018-01-24 20:51:03 +0000

    Added delete shortende url functionality

--- express_server.js
+++ express_server.js
@@ -2,12 +2,24 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+// function saveURL(shortURL, longURL) {
+//   urlDatabase[shortURL] = longURL;
+// }
+
+// function loadURL(shortURL) {
+//   return urlDatabase[shortURL];
+// }
+
+
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -26,17 +38,42 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
+// deleting an element in database
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
+});
+
+
 app.get("/urls/:id", (req, res) => {
-    let templateVars = { shortURL: req.params.id };
+    let templateVars = { 
+      shortURL: req.params.id, 
+      longURL: urlDatabase[req.params.id] 
+    };
     res.render("urls_show", templateVars);
   });
-
-// POST REQUEST ///////////
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// loading data from database
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = loadURL(req.params.shortURL);
+  res.redirect(longURL);
+});
 
+// storing data in database
+app.post("/urls", (req, res) => {
+  const value = req.body.longURL;
+  const key = createRandomString(6);
+  saveURL(key, value);
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 
 
@@ -44,5 +81,13 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+
+function createRandomString (length) {
+  var str = '';
+  for (var i = 0; i < length; i++) {
+      str += Math.random().toString(36).substr(2);
+      return str.substr( 0, length );
+  }
+}
 
 
